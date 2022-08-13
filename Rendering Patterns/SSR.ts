b7e0524aaa53c942f546398b5bc545f0d3af900b
ChrisCooper0 <christopher.cooper@mail.com>
2022-08-13 08:57:04 +0100

    update SSR example

--- Rendering Patterns/SSR.ts
+++ Rendering Patterns/SSR.ts
@@ -7,4 +7,14 @@ SSR generates the full HTML for the page content to be rendered in response to a
 
 Less JavaScript shipped to the client browser leads to quicker FCP and TTI (KPI)
 
+SSR with Next.js
+
+The Next.js framework also supports SSR. This pre-renders a page on the server on every request. It can be accomplished by exporting an async function called getServerSideProps() from a page as follows.
+
+export async function getServerSideProps(context) {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
+The context object contains keys for HTTP request and response objects, routing parameters, querystring, locale, etc.
 */
